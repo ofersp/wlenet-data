66d1fcd2940bb38aa7de278ddf44b46877b11373
Ofer Springer <ofer.springer@gmail.com>
2019-03-02 15:47:05 +0200

    added wget_simulations.sh

---
+++ simulation/wget_simulations.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+wget https://zenodo.org/record/1401224/files/clash_003_test.tar.gz?download=1
+tar xvzf clash_003_test.tar.gz.tar.gz
+rm clash_003_test.tar.gz.tar.gz
+
+wget https://zenodo.org/record/1401224/files/clash_004_train.tar.gz?download=1
+tar xvzf clash_004_train.tar.gz
+rm clash_004_train.tar.gz
+
+wget https://zenodo.org/record/1401224/files/hig_002.tar.gz?download=1
+tar xvzf hig_002.tar.gz
+rm hig_002.tar.gz
+
+wget https://zenodo.org/record/1401224/files/hig_004.tar.gz?download=1
+tar xvzf hig_004.tar.gz
+rm hig_004.tar.gz
+
+wget https://zenodo.org/record/1401224/files/low_002.tar.gz?download=1
+tar xvzf low_002.tar.gz
+rm low_002.tar.gz
+
+wget https://zenodo.org/record/1401224/files/low_004.tar.gz?download=1
+tar xvzf low_004.tar.gz
+rm low_004.tar.gz
+
+wget https://zenodo.org/record/1401224/files/med_002.tar.gz?download=1
+tar xvzf med_002.tar.gz
+rm med_002.tar.gz
+
+wget https://zenodo.org/record/1401224/files/med_004.tar.gz?download=1
+tar xvzf med_004.tar.gz
+rm med_004.tar.gz
