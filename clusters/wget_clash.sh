24fe72cac4c9f0482ad5c55fe187bee2723800b1
Ofer Springer <ofer.springer@gmail.com>
2019-03-02 15:30:38 +0200

    trying to make wget_clash.sh work on colab by switching to bash

--- clusters/wget_clash.sh
+++ clusters/wget_clash.sh
@@ -1,46 +1,46 @@
-#!/bin/tcsh
+#!/bin/bash
 
-set basedir = `pwd`/clash
+set BASE_DIR = `pwd`/clash
 
-foreach cluster (`cat clash_cluster_names.txt`)
-    set clusterdir = ${basedir}/${cluster}
+for CLUSTER in (`cat clash_cluster_names.txt`); do
+    set CLUSTER_DIR = ${BASE_DIR}/${CLUSTER}
 
-    mkdir -p ${clusterdir}/data/hst/scale_65mas
-    mkdir -p ${clusterdir}/catalogs/hst
-    mkdir -p ${clusterdir}/catalogs/wl
-    mkdir -p ${clusterdir}/catalogs/molino
+    mkdir -p ${CLUSTER_DIR}/data/hst/scale_65mas
+    mkdir -p ${CLUSTER_DIR}/catalogs/hst
+    mkdir -p ${CLUSTER_DIR}/catalogs/wl
+    mkdir -p ${CLUSTER_DIR}/catalogs/molino
 
-    cd ${clusterdir}/catalogs/hst
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/catalogs/hst/hlsp_clash_hst_catalog_readme.txt
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/catalogs/hst/hlsp_clash_hst_${cluster}_zeropoints.txt
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/catalogs/hst/hlsp_clash_hst_ir_${cluster}_cat.txt
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/catalogs/hst/hlsp_clash_hst_acs-ir_${cluster}_cat.txt
+    cd ${CLUSTER_DIR}/catalogs/hst
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/catalogs/hst/hlsp_clash_hst_catalog_readme.txt
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/catalogs/hst/hlsp_clash_hst_${CLUSTER}_zeropoints.txt
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/catalogs/hst/hlsp_clash_hst_ir_${CLUSTER}_cat.txt
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/catalogs/hst/hlsp_clash_hst_acs-ir_${CLUSTER}_cat.txt
 
-    cd ${clusterdir}/catalogs/molino
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/catalogs/molino/hlsp_clash_hst_ir_${cluster}_cat-molino.txt
+    cd ${CLUSTER_DIR}/catalogs/molino
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/catalogs/molino/hlsp_clash_hst_ir_${CLUSTER}_cat-molino.txt
 
-    cd ${clusterdir}/data/hst
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/hlsp_clash_hst_${cluster}_readme.txt
+    cd ${CLUSTER_DIR}/data/hst
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/hlsp_clash_hst_${CLUSTER}_readme.txt
 
-    cd ${clusterdir}/data/hst/scale_65mas
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f435w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f435w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f475w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f475w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f606w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f606w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f625w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f625w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f775w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f775w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f814w_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f814w_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f850lp_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_f850lp_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_total_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs_${cluster}_total_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_wfc3ir_${cluster}_total_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_wfc3ir_${cluster}_total_v1_wht.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs-wfc3ir_${cluster}_total_v1_drz.fits
-    wget https://archive.stsci.edu/missions/hlsp/clash/${cluster}/data/hst/scale_65mas/hlsp_clash_hst_acs-wfc3ir_${cluster}_total_v1_wht.fits
-end
+    cd ${CLUSTER_DIR}/data/hst/scale_65mas
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f435w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f435w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f475w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f475w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f606w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f606w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f625w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f625w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f775w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f775w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f814w_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f814w_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f850lp_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_f850lp_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_total_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs_${CLUSTER}_total_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_wfc3ir_${CLUSTER}_total_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_wfc3ir_${CLUSTER}_total_v1_wht.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs-wfc3ir_${CLUSTER}_total_v1_drz.fits
+    wget https://archive.stsci.edu/missions/hlsp/clash/${CLUSTER}/data/hst/scale_65mas/hlsp_clash_hst_acs-wfc3ir_${CLUSTER}_total_v1_wht.fits
+done
